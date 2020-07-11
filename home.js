c4277e4b4ce281fdcc1e12e88b76539fe7421dc7
PRASHANT KUMAR <prashantkumardagur@gmail.com>
2020-07-11 17:59:20 +0530

    dark mode added

--- home.js
+++ home.js
@@ -1,4 +1,42 @@
+var darkCheck = localStorage.getItem("darkMode");
+
+if(darkCheck === null){ 
+    darkMode = false; 
+} else {
+    if(darkCheck === "true") darkMode = true;
+    else darkMode = false;
+}
+
+if(darkMode) { toggleDarkMode(); }
+
 $("#hidebtn").click(function(){ $(".maintenance").hide(); });
 $("#back").click(function(){ history.back(); });
+$("#topbtn").click(function(){ gotoTop(); });
+$("#darkbtn").click(function(){ 
+    darkMode = !darkMode;
+    toggleDarkMode();
+});
+
+function toggleDarkMode(){
+    localStorage.setItem("darkMode", darkMode);
+    $(".theme").toggleClass("dark");
+    if(darkMode) $("body").css("background-color", "#111");
+    else $("body").css("background-color", "#f5f5f5");
+}
 
 $(".ctnum").css("border-color", $('.accentbg').css("background-color"));
+
+var controls = document.getElementById("controls");
+
+window.onscroll = function(){ scrollfunc(); }
+function scrollfunc(){
+    if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
+        controls.style.bottom = "0px";
+      } else {
+        controls.style.bottom = "-65px";
+      }
+}
+function gotoTop(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
