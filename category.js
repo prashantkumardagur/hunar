4e87649a2c93edad35ab3e4b291b2175e928d30b
PRASHANT KUMAR <prashantkumardagur@gmail.com>
2020-07-13 23:15:28 +0530

    main

---
+++ category.js
@@ -0,0 +1,92 @@
+var categoryContent = [
+    {
+        name: "Dance",
+        image: ["sideimages/dance.jpg", "sideimages/dance-mobi.jpg"],
+        paragraph: "Dance is a means to communicate your emotions without really saying anything much. Each and every person has emotions but only few have the ability to express and this uniqueness of converting your emotions becomes a Talent that one possesses. <br><br> We have come up with this wonderful opportunity to showcase your talent world-wide and furnish your talent by competing with the participants across the nation. <br><br> Hunar 3.0 is one such platform that is versatile in nature and where talent meets opportunity. Register now to participate in this Mega Event of the year!",
+        rules: "<li>Entry fees Rs 100/Person.</li><li>There is no age category, anyone can participate.</li><li>We accept all styles of dance form.</li><li>You can perform solo, duo, trio or in a group.</li><li>If you are participating in a group then each member of the group have to register solely and then form a group. No group registration is allowed.</li><li>You have to send the freshly recorded video with no editing. There should be no retake or cuts in between the video.</li><li>The duration of the dance should be between 1:00 min to 1:30 min.</li>",
+        judges: '<div class="categoryJudges"><img src="guests/rupesh.jpg" alt="guest"><h6>Rupesh Bane</h6></div>'
+    },
+    {
+        name: "Singing",
+        image: ["sideimages/singing.jpg", "sideimages/singing-mobi.jpg"],
+        paragraph: "The most important part about a singer or Rapper is being passionate. We believe passion is the driving factor that makes everything worthwhile. If there is no passion, there is no point in pursuing that talent. When a singer is performing, the passion is visible. The best singers and rappers passion is what makes them so amazing. After the talent, creativity, ambition, pride, inspiration and beauty is evident, the passion brings it all together. <br><br> If you have that passion in you then we are here with the biggest opportunity to showcase your talent by organizing a national level competition Hunar 3.0 where you will get recognition while competing with others talents across the nation. <br><br> We believe talents should not be confined it should be embraced and reach the right place where it is valued. You can register now to participate in this Mega event of the year!",
+        rules: "<li>Entry fees Rs 100/Person.</li><li>There is no age category, anyone can participate.</li><li>We accept all  singing styles including art music such as opera and Chinese opera, Indian music and religious music styles such as gospel, traditional music styles, world music, jazz, blues, ghazal and popular music styles such as pop, rock and electronic dance music.</li><li>You can sing or do rapping in any language.</li><li>You can perform solo, duo, trio or in a group.</li><li>If you are participating in a group means more than one participants, then each member of the group have to register solely and then form a group. No group registration is allowed.</li><li>You have to send the freshly recorded video with no editing. There should be no retake or cuts in between the video.</li><li>The duration of the video should be between 1:00 min to 1:30 min.</li>",
+        judges: '<div class="categoryJudges"><img src="guests/farhan.jpg" alt="guest"><h6>Farhan Sabbir</h6></div><div class="categoryJudges"><img src="guests/sunny.jpg" alt="guest"><h6>Sunny Hindustani</h6></div><div class="categoryJudges"><img src="guests/amaan.jpg" alt="guest"><h6>Amaan Khan</h6></div>'
+    },
+    {
+        name: "Modelling",
+        image: ["sideimages/modelling.jpg", "sideimages/modelling-mobi.jpg"],
+        paragraph: "This is one such talent that each and every one possess. Modelling is the way of presenting yourself. Models are known as the face of fashion. The only things that is needed is confidence of presenting your style in front of the audience. <br><br> If you possess such talent then we are here to provide you with the right platform to embrace this talent and live your dreams by organising a versatile competition at National Level Hunar 3.0. Register yourself now to become a part of Mega event of the year!",
+        rules: "<li>Entry fees Rs 100/Person.</li><li>There is no age category, anyone can participate.</li><li>We accept all types of fashion and additionally you can also represent your culture, or your religion or your state.</li><li>Candidates must conduct themselves in a proper manner and portray high moral standards. </li><li>You have to send the freshly recorded video with no editing. There should be no retake or cuts in between the video.</li><li>The duration of the walk should be between 1:00 min to 1:30 min.</li>",
+        judges: '<div class="categoryJudges"><img src="guests/sameer.jpg" alt="guest"><h6>Sameer Mark</h6></div>'
+    },
+    {
+        name: "Skit / Mime",
+        image: ["sideimages/mime.jpg", "sideimages/mime-mobi.jpg"],
+        paragraph: "Skit and mime involves acting out a story through body motions, without the use of speech. In this artist is a character in a film or skit without sound. It requires lots of understanding and practices and only few persist this kind of talent. If you have such talent then we are here to furnish your talent and represent it world-wide by providing you one such platform where you can showcase your talent by taking part in a versatile competition Hunar 3.0. <br><br> Register now to be a part of the Mega Event of the Year!",
+        rules: "<li>Entry fees Rs 100/Person.</li><li>There is no age category, anyone can participate.</li><li>You can perform solo, duo, trio or in a group.</li><li>If you are participating in a group then each member of the group have to register solely and then form a group. No group registration is allowed.</li><li>You have to send the freshly recorded video with no editing. There should be no retake or cuts in between the video.</li><li>The duration of the show should be between 1:00 min to 1:30 min.</li>",
+        judges: '<div class="categoryJudges"><img src="guests/shrabani.jpg" alt="guest"><h6>Shrabani Deodhar</h6></div>'
+    },
+    {
+        name: "Beat-boxing",
+        image: ["sideimages/beatbox.jpg", "sideimages/beatbox-mobi.jpg"],
+        paragraph: "Beatboxing is an extraordinary art of vocal percussion primarily involving the art of mimicking drum machines, using one's mouth, lips, tongue, and voice. It may also involve vocal imitation of turntablism, and other musical instruments. <br><br> The beatbox often clubs with DJs or Rap/Hip hop artists. There have been and are many famous artists who have mastered this art. <br><br> If you are also one of them and have this talent then we are here with the opportunity to showcase this talent at big level by organizing a versatile National Level Competition Hunar 3.0. <br><br> Register yourself now and participate in a Mega event of the Year!",
+        rules: "<li>Entry fees Rs 100/Person.</li><li>There is no age category, anyone can participate.</li><li>You can perform solo, duo, trio or in a group.</li><li>If you are participating in a group then each member of the group have to register solely and then form a group. No group registration is allowed.</li><li>You have to send the freshly recorded video with no editing. There should be no retake or cuts in between the video.</li><li>The duration of the video should be between 1:00 min to 1:30 min.</li>",
+        judges: '<div class="categoryJudges"><img src="guests/aa.jpeg" alt="guest"><h6>Aman & Adil</h6></div><div class="categoryJudges"><img src="guests/deepak.png" alt="guest"><h6>Deepak Soni</h6></div>'
+    },
+    {
+        name: "Photography",
+        image: ["sideimages/photography.jpg", "sideimages/photography-mobi.jpg"],
+        paragraph: "Photography is a very special art that represents your connection with the world around you, a means of you to tell the story. It is the art of observation that a photographer can see in especially in most normal things. <br><br> Everyone is a photographer but only few have the capability to describe their view of looking around and capturing the emotions, beauty around us all and here this art becomes a talent that only few people among whole have. We believe if one have this amazing talent then we should provide them the opportunity to showcase their talent to the world and talent should not be confined. <br><br> So we have come up with the idea of this National level competition Hunar 3.0 to carry on with your dreams and learn by competing with the talents across the nation. Register now and be a part of Mega Event of the Year!!",
+        rules: "<li>Entry fees Rs 100/Person.</li><li>There is no age category, anyone can participate.</li><li>We accept all kinds of Photography such as adventure, candid, cityscape etc.</li><li>Photos submitted must be at least 640 pixels on the shorter side, and no more than 2000 pixels on the longer side. Images should be no larger than 2MB. Photos must be in JPEG format.</li><li>Basic editing, including colour enhancement, the use of filters, and cropping of the Photo(s) is acceptable, provided any such editing does not affect the authenticity and/or genuineness of the Photo(s).</li><li>Advanced editing used to create illusions, deceptions and/or manipulations, and the adding and removing of significant elements within the frame is prohibited.</li><li>Photos that portray or otherwise include inappropriate and/or offensive content, including provocative nudity, violence, human rights and/or environmental violation, and/or any other contents deemed to be contrary to the law, religious, cultural & moral traditions and practices of India, are strictly prohibited and will be immediately discarded.</li>",
+        judges: '<div class="categoryJudges"><img src="guests/profile.png" alt="guest"><h6>Celeb Name</h6></div>'
+    },
+    {
+        name: "Poetry",
+        image: ["sideimages/poetry.jpg", "sideimages/poetry-mobi.jpg"],
+        paragraph: "Poetry telling is medium of telling your thoughts, feelings, imaginations to the listener. It allows listener to experience real feelings of the teller. It’s very difficult to express your feelings, thoughts in front of others but some peoples are blessed with this talent. <br><br> We believe this kind of talent should be recognized at big level so we came up with a mega Event Hunar 3.0 that is a versatile competition to provide platform to such talents. <br><br> Register now to Participate in a mega Event of the Year!",
+        rules: "<li>Entry fees Rs 100/Person.</li><li>There is no age category, anyone can participate.</li><li>You can use any language in your Poetry.</li><li>The Poetry should be of your own, it should not be copied from anywhere else.</li><li>You have to send the freshly recorded video with no editing. There should be no retake or cuts in between the video.</li><li>The duration of the poetry should be between 1:00 min to 1:30 min.</li>",
+        judges: '<div class="categoryJudges"><img src="guests/profile.png" alt="guest"><h6>Celeb Name</h6></div>'
+    },
+    {
+        name: "Painting",
+        image: ["sideimages/painting.jpg", "sideimages/painting-mobi.jpg"],
+        paragraph: "A sketch is a rapidly executed freehand drawing that is not usually intended as a finished work. A sketch may serve a number of purposes: it might record something that the artist sees, it might record or develop an idea for later use or it might be used as a quick way of graphically demonstrating an image, idea and Painting is not just an expressing but from time, a thumb print of history, and in a painting, if one listens, and observe not just through eyes but the heart, they will hear how life began. <br><br> If you have such beautiful art then go ahead and participate and let your talent shine by participating in the National level Competition Hunar 3.0. Register now to be a part of the Mega Event of the Year!",
+        rules: "<li>Entry fees Rs 100/Person.</li><li>There is no age category, anyone can participate.</li><li>We accept all kinds of theme for your art.</li><li>Both 2D and 3D work is eligible.</li><li>You can use any types of colour like oil, water colour, pastel, acrylic, etc.</li><li>Paintings must be created in physical media (no digital 'paintings').</li><li>You may only submit your own artwork that you, personally created.</li><li>File submitted must be in a jpeg format.</li>",
+        judges: '<div class="categoryJudges"><img src="guests/profile.png" alt="guest"><h6>Celeb Name</h6></div>'
+    },
+    {
+        name: "Instrument Playing",
+        image: ["sideimages/instrument.jpg", "sideimages/instrument-mobi.jpg"],
+        paragraph: "Music without instrument is not possible. Instrument gives life to music and many are blessed to have such talents in them. <br><br> If you also possess such talent then this is the right opportunity for you to showcase your talent and get recognized by participating in one and only national level competition HUNAR 3.0, where talent meets opportunity.",
+        rules: "<li>Entry fees Rs 100/Person.</li><li>There is no age category, anyone can participate.</li><li>You can perform solo, duo, trio or in a group.</li><li>If you are participating in a group then each member of the group have to register solely and then form a group. No group registration is allowed.</li><li>You have to send the freshly recorded video with no editing. There should be no retake or cuts in between the video.</li><li>The duration of the video should be between 1:00 min to 1:30 min.</li>",
+        judges: '<div class="categoryJudges"><img src="guests/profile.png" alt="guest"><h6>Celeb Name</h6></div>'
+    },
+    {
+        name: "Story-telling",
+        image: ["sideimages/storytelling.jpg", "sideimages/storytelling.jpg"],
+        paragraph: "Storytelling is the interactive art of using words and actions to reveal the elements and images of a story while encouraging the listener’s imagination. In storytelling, the listener imagines the story and becomes, therefore, a co-creator of the story as experienced. <br><br> If you have such talent to let people experience your story in your way then you are welcomed to our competition Hunar 3.0 as we believe expressing your thoughts is a bit difficult task and is not every one’s cup of tea and we are happy to provide platform to such talents so that they can furnish their talents and people recognize their extraordinary talents.",
+        rules: "<li>Entry fees Rs 100/Person.</li><li>There is no age category, anyone can participate.</li><li>You can use any language in your Story.</li><li>We accept both fictious and non-fictious stories.</li><li>The Story should be of your own, it should not be copied from anywhere else.</li><li>You have to send the freshly recorded video with no editing. There should be no retake or cuts in between the video.</li><li>The duration of the story should be between 1:00 min to 1:30 min.</li>",
+        judges: '<div class="categoryJudges"><img src="guests/shrabani.jpg" alt="guest"><h6>Shrabani Deodhar</h6></div>'
+    },
+    {
+        name: "Comedy",
+        image: ["sideimages/comedy.jpg", "sideimages/comedy-mobi.jpg"],
+        paragraph: "Comedy is the special talent that is vulnerable and only few possess this kind of talent. Using your actions, words, gestures to make someone laugh is an extraordinary talent. We all are here to live our life happily but life is not the same all the time. We need some entertainment to keep ourselves out of any difficult situation. Comedy is one such talent that refreshes our mind but it's not an easy task too. <br><br> If you possess such talent then participate in our Mega event HUNAR 3.0. and get your talent  recognized at national level.",
+        rules: "<li>Entry fees Rs 100/Person.</li><li>There is no age category, anyone can participate.</li><li>You can perform solo, duo, trio or in a group.</li><li>If you are participating in a group then each member of the group have to register solely and then form a group. No group registration is allowed.</li><li>You have to send the freshly recorded video with no editing. There should be no retake or cuts in between the video.</li><li>The duration of the video should be between 1:00 min to 1:30 min.",
+        judges: '<div class="categoryJudges"><img src="guests/profile.png" alt="guest"><h6>Celeb Name</h6></div>'
+    }
+];
+
+function expandCategory(x){
+    if(window.innerWidth<500) $(".sideImage").attr("src", categoryContent[x].image[1]);
+    else $(".sideImage").attr("src", categoryContent[x].image[0]);
+    $(".sideInfo h5").html("Category " + (x+1));
+    $(".sideInfo h4").html(categoryContent[x].name);
+    $(".sideInfo p").html(categoryContent[x].paragraph);
+    $(".sideInfo ul").html(categoryContent[x].rules);
+    $(".sideInfo section").html(categoryContent[x].judges);
+    $("#categoryScreen").css("bottom", "5vh");
+}
+$("#minimizeCategory").click(function(){ $("#categoryScreen").css("bottom", "-120vh"); });
+
